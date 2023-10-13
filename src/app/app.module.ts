4bce671264a916cd55915361330da5351fdd167f
Alessandro <alessandroRetuerto@hotmail.com>
2023-10-13 12:17:56 -0500

    add modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
+import { SharedModule } from './shared/shared.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, SharedModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
